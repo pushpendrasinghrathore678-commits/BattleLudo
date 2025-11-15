ec70ad0877c91053d16a43a8b9cd60a1a3a64e1d
pushpendrasinghrathore678-commits <pushpendrasinghrathore678@gmail.com>
2025-11-15 23:52:02 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Battle Ludo</h1>
+      <p>Welcome to your Ludo game!</p>
+    </div>
+  );
+}
